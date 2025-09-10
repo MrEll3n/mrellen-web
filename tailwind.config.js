1c34b312abeb570dd12f967dfc2e03937edf34cc
Novotny <vnovotny@GK-DOMAIN>
2025-09-10 14:29:33 +0200

    refactor: redone styles via tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,10 @@
-module.exports = {
+export default {
     content: ["./src/**/*.{astro,js,ts,jsx,tsx}"],
     theme: {
         extend: {
-            keyframes: {
-                "pulse-blur": {
-                    "0%, 100%": {
-                        transform: "scale(1)",
-                        opacity: "0.7",
-                        filter: "blur(80px)",
-                    },
-                    "50%": {
-                        transform: "scale(1.1)",
-                        opacity: "1",
-                        filter: "blur(80px)",
-                    },
-                },
-            },
-            animation: {
-                "pulse-blur": "pulse-blur 6s ease-in-out infinite",
-            },
             fontFamily: {
                 poppins: ["Poppins", "sans-serif"],
+                jetbrains: ["JetBrains Mono", "monospace"],
             },
         },
     },
